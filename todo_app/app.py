ea68304ac64a6cdae23931eb5f3dbd3af844ca5c
Ryan Kelly <ryan.kelly@asda.co.uk>
2023-10-17 15:13:56 +0100

    add items working but left aligned too far

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 
 from todo_app.flask_config import Config
 from todo_app.data.session_items import get_items
+from todo_app.data.session_items import add_item
 
 app = Flask(__name__)
 app.config.from_object(Config())
@@ -10,5 +11,10 @@ app.config.from_object(Config())
 @app.route('/')
 def index():
     items = get_items()
-    return render_template('index.html', my_items = items)
-    
+    return render_template('index.html', items = items)
+
+@app.route('/add_todo_item', methods = ['POST'])
+def add_todo():
+    new_todo_title = request.form.get('title')
+    add_item(new_todo_title)
+    return redirect('/')
