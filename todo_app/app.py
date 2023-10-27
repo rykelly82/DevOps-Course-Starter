cdaf050843b652ba61c3d4cfc7103375a571375a
Ryan Kelly <ryan.kelly@asda.co.uk>
2023-10-27 13:38:31 +0100

    Minor Changes for indention and code tidy

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,8 +1,7 @@
 from flask import Flask, render_template, redirect, request
 
 from todo_app.flask_config import Config
-from todo_app.data.session_items import get_items
-from todo_app.data.session_items import add_item
+from todo_app.data.session_items import get_items, add_item
 
 app = Flask(__name__)
 app.config.from_object(Config())
