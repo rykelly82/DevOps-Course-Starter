30632c1ee449eac5fda8a96ad9f5d11fdd1f54b5
Ryan Kelly <ryan.kelly@asda.co.uk>
2023-11-17 15:59:50 +0000

    display items from Trello

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,8 +1,7 @@
 from flask import Flask, render_template, redirect, request
 
 from todo_app.flask_config import Config
-from todo_app.data.session_items import get_items
-from todo_app.data.trello_items import add_item
+from todo_app.data.trello_items import add_item, get_items
 
 app = Flask(__name__)
 app.config.from_object(Config())
