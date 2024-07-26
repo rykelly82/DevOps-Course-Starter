f3ada72159f2bed112e0b4b6f1ee0c0429de9c87
Ryan Kelly <ryan.kelly@asda.co.uk>
2024-07-26 15:58:29 +0000

    setup and demo

--- pymongo_demo.py
+++ pymongo_demo.py
@@ -6,4 +6,23 @@ dotenv.load_dotenv()
 
 client = pymongo.MongoClient(os.getenv("MONGO_CONNECTION_STRING"))
 
-print(client.list_database_names())
+db =client.todoappdatabase
+
+collection = db.todo_items
+
+# Insert a record
+collection.insert_one({"description": "A pymongo document"})
+
+# Read all records
+
+list(collection.find())
+
+
+# Insert and update a record
+collection.insert_one({"description": "An updateable document document", "type": "updateable"})
+collection.update_one({"type": "updateable"}, {"$set": {"description": "CHANGED!!!"}})
+
+
+
+
+print(collection)
