da7bbb51b6e93a540d8cbf56601ae6a3ebdd959c
Ryan Kelly <ryan.kelly@asda.co.uk>
2024-07-26 14:31:12 +0000

    start exercise 10

---
+++ pymongo_demo.py
@@ -0,0 +1,5 @@
+import pymongo
+
+client = pymongo.MongoClient(mongodb://ryanmongoaccount:WwyBPAEpwGM5pmD5hbCBv7SC3AO8zi8C9J00nn0EixmOf6tw1yWNkTEaO1wTvzPOMoWtGKJj8ACaACDbSEzERg==@ryanmongoaccount.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@ryanmongoaccount@)
+
+client.list
