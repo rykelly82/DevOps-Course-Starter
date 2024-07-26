7378a9d1f148965b7a845c0eb712d220c75c7f22
Ryan Kelly <ryan.kelly@asda.co.uk>
2024-07-26 14:43:08 +0000

    set up exercise 10

--- pymongo_demo.py
+++ pymongo_demo.py
@@ -1,5 +1,9 @@
 import pymongo
+import dotenv
+import os
 
-client = pymongo.MongoClient(mongodb://ryanmongoaccount:WwyBPAEpwGM5pmD5hbCBv7SC3AO8zi8C9J00nn0EixmOf6tw1yWNkTEaO1wTvzPOMoWtGKJj8ACaACDbSEzERg==@ryanmongoaccount.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@ryanmongoaccount@)
+dotenv.load_dotenv()
 
-client.list
+client = pymongo.MongoClient(os.getenv("MONGO_CONNECTION_STRING"))
+
+print(client.list_database_names())
