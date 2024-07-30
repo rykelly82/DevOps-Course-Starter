adbe017ca1758aa95110acfc5675142c6d8b4782
Ryan Kelly <ryan.kelly@asda.co.uk>
2024-07-30 10:22:18 +0000

    Migrate viewing of items from Trello to Mongo DB

--- todo_app/data/mongo_items.py
+++ todo_app/data/mongo_items.py
@@ -1,6 +1,8 @@
 import pymongo
 import os
 
+from todo_app.data.item import Item
+
 client = pymongo.MongoClient(os.getenv("MONGO_CONNECTION_STRING"))
 
 db =client[os.getenv("MONGO_DATABASE_NAME")]
@@ -18,7 +20,16 @@ def add_item(new_todo_title: str):
     collection.insert_one(new_item)
 
 def get_items():
-    pass
+    mongodb_documents = list(collection.find())
+
+    items = []
+    
+    for document in mongodb_documents:
+        item = Item.from_mongo_document(document)
+        items.append(item)
+
+    return items
+
 
 def move_item_to_done():
     pass
